21ec0de02df82ab79f5b4849fc95acce3208958b
tlevin <toddlevin802@gmail.com>
2016-02-18 14:30:04 -0800

    Update with Things to do page, style changes, etc

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 // Timer for front page
 
-var today = moment()
-var wedding = moment("2016 06 04", "YYYY MM DD")
-$('.weddingDate').text(wedding.format('dddd, MMMM Do, YYYY'))
+// var today = moment()
+// var wedding = moment("2016 06 04", "YYYY MM DD")
+// $('.weddingDate').text(wedding.format('dddd, MMMM Do, YYYY'))
